3a39a9eed7a00c18c865d7b8d424057aaaee651c
Pavlo Kletsko <pkletsko@gmail.com>
2024-04-30 10:12:53 +0200

    fix alg naming, because they were mixed up

---
+++ algorithms/dp/leetcode/Group 2: Linear Sequences with Constant Transition/MinMax/2D/983. Minimum Cost For Tickets/From_0_To_TheEnd.js
@@ -0,0 +1,70 @@
+/**
+ You have planned some train traveling one year in advance. The days of the year in which you will travel are given as an integer array days. Each day is an integer from 1 to 365.
+ Train tickets are sold in three different ways:
+
+ a 1-day pass is sold for costs[0] dollars,
+ a 7-day pass is sold for costs[1] dollars, and
+ a 30-day pass is sold for costs[2] dollars.
+ The passes allow that many days of consecutive travel.
+
+ For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
+ Return the minimum number of dollars you need to travel every day in the given list of days.
+
+ Example 1:
+
+ Input: days = [1,4,6,7,8,20], costs = [2,7,15]
+ Output: 11
+ Explanation: For example, here is one way to buy passes that lets you travel your travel plan:
+ On day 1, you bought a 1-day pass for costs[0] = $2, which covered day 1.
+ On day 3, you bought a 7-day pass for costs[1] = $7, which covered days 3, 4, ..., 9.
+ On day 20, you bought a 1-day pass for costs[0] = $2, which covered day 20.
+ In total, you spent $11 and covered all the days of your travel.
+ Example 2:
+
+ Input: days = [1,2,3,4,5,6,7,8,9,10,30,31], costs = [2,7,15]
+ Output: 17
+ Explanation: For example, here is one way to buy passes that lets you travel your travel plan:
+ On day 1, you bought a 30-day pass for costs[2] = $15 which covered days 1, 2, ..., 30.
+ On day 31, you bought a 1-day pass for costs[0] = $2 which covered day 31.
+ In total, you spent $17 and covered all the days of your travel.
+
+ Constraints:
+
+ 1 <= days.length <= 365
+ 1 <= days[i] <= 365
+ days is in strictly increasing order.
+ costs.length == 3
+ 1 <= costs[i] <= 1000
+ */
+
+export function mincostTickets(days, costs) {
+    let memo = Array.from(Array(days.length), ()=> Array.from({ length: days.length * 30 }, () => -1));
+    // path construction from 0 to Target Begin [-------->] End
+    // memo will be filled:              Begin [<--------] End
+
+    const ticket = [1, 7, 30];
+
+    function dfs(index, coveredUntilDay) {
+        if (index >= days.length) {
+            return 0;
+        }
+
+        if (memo[index][coveredUntilDay] !== -1) {
+            return memo[index][coveredUntilDay];
+        }
+
+        let min = Infinity;
+
+        if (coveredUntilDay >= days[index]) {
+            min = Math.min(min, dfs(index + 1, coveredUntilDay));
+        }
+
+        for(let i = 0; i < costs.length; i++) {
+            min = Math.min(min, dfs(index + 1, days[index] - 1 + ticket[i]) + costs[i]);
+        }
+
+        return memo[index][coveredUntilDay] = min;
+    }
+
+    return dfs(0,0);
+}
